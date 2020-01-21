a123a64f16f5c6f103eb24d68d81573c95931a86
alex <alex@fayers.com>
2020-01-21 12:02:00 +0000

    Basic UI, input validation, and (I think) all of the specification functionality

--- affine.py
+++ affine.py
@@ -1,63 +1,158 @@
 import math
 
-def coprime(a,b): # check if a and b are coprime using math module
-	return math.gcd(a,b) == 1
 
-def coprimeGen(n): # find coprimes of a number 'n'
+####################################################
+#
+#   Math stuff
+#
+####################################################
 
-	# validate n is number
 
-	if n < 0: # don't check if number is too small
-		return []
-	else:
-		coprimesTemp = []
-		for toCheck in range(1,n,2): # don't check even numbers
-			if coprime(toCheck,n):
-				coprimesTemp.append(toCheck)
-		return coprimesTemp
+def inverse_mod(a, maxNo):  # find inverse modulo of a number
+    for b in range(1, maxNo, 2):  # don't process even numbers
+        if ((a * b) % maxNo) == 1:
+            return b
+    raise ValueError(str(a)+" has no inverse mod"+str(maxNo))
 
-def getInputs():
-	a = 5 #int(input("Enter a: "))
-	b = 8 #int(input("Enter b: "))
 
-	return (a,b)
+def coprime(a, b):  # check if a and b are coprime using math module
+    return math.gcd(a, b) == 1
 
-def encryptSingle(plainChar, a, b, keyspace, encrypt): # str, int, int, str, bool
-	try:
-		x = keyspace.index(plainChar)
-	except:
-		print("Encountered invalid char! '"+str(x)+"'")
-		return '!'
-	# ensure char is lowercase
-	
-	if encrypt:	
-		encryptedX = (a*x + b) % len(keyspace)
-	else:
-		encryptedX = (a*x - b) % len(keyspace)
-	
-	encryptedChar = keyspace[encryptedX]	
 
-	# make encrypted char uppercase?
+def coprime_gen(n):  # find coprimes of a number 'n'
+    if n < 0:  # don't check if number is too small
+        return []
+    else:
+        coprimesTemp = []
+        for toCheck in range(1, n, 2):  # don't check even numbers
+            if coprime(toCheck, n):
+                coprimesTemp.append(toCheck)
+        return coprimesTemp
 
-	return encryptedChar
 
-def encryptString(plainText, a, b, keyspace, encrypt):
-	encryptedText = ""
-	
-	for character in plainText:
-		encryptedText += encryptSingle(character, a, b, keyspace,encrypt)
-	
-	return encryptedText
+####################################################
+#
+#   Input stuff
+#
+####################################################
+
+
+def get_inputs(keyspace, coprimes):
+    getInput = True
+    while getInput:
+        print("Encrypt (e) or decrypt (d)?")
+        encrypt_decrypt = input("Option: ")
+
+        if encrypt_decrypt.lower() == 'e':
+            encrypt_decrypt = 'encrypt'
+            getInput = False
+
+        elif encrypt_decrypt.lower() == 'd':
+            encrypt_decrypt = 'decrypt'
+            getInput = False
+
+        else:
+            print("'"+str(encrypt_decrypt)+"' is an invalid option!")
+
+    getInput = True
+    while getInput:
+        a = input("First key (a) (must be coprime with "+str(len(keyspace))+"): ")
+
+        try:
+            a = int(a)
+
+            if a not in coprimes:
+                print("Please enter an integer that is coprime with " + str(len(keyspace)))
+                print("E.g: " + str(coprimes))
+            else:
+                getInput = False
+        except ValueError:
+            print("Invalid number! Please enter an integer")
+
+    getInput = True
+    while getInput:
+        b = input("Second key (b): ")
+
+        try:
+            b = int(b)
+            getInput = False
+        except ValueError:
+            print("Invalid number! Please enter an integer")
+
+    message = input("Message to "+str(encrypt_decrypt)+": ")
+
+    return a, b, encrypt_decrypt, message
+
+
+####################################################
+#
+#   Encryption and decryption stuff
+#
+####################################################
+
+
+def encrypt_decrypt_single(inputChar, a, b, keyspace, encrypt):  # str, int, int, str, str
+    if inputChar == ' ':  # check for spaces
+        return ' '
+
+    try:
+        x = keyspace.index(inputChar)
+    except ValueError:  # if current char is unrecognised, remove it (as per spec)
+        return ''
+
+    # ensure char is lowercase
+
+    if encrypt == 'encrypt':
+        output_x = (a * x + b) % len(keyspace)
+
+    elif encrypt == 'decrypt':
+        inverse_a = inverse_mod(a, len(keyspace))
+        output_x = (inverse_a * (x - b)) % len(keyspace)
+
+    else:
+        raise SyntaxError("Error in encrypt/decrypt variable. Check spelling?")
+
+    outputChar = keyspace[output_x]
+
+    # make encrypted char uppercase?
+
+    return outputChar
+
+
+def encrypt_decrypt_string(plainText, a, b, keyspace, encrypt):
+    encryptedText = ""
+
+    for character in plainText:
+        encryptedText += encrypt_decrypt_single(character, a, b, keyspace, encrypt)
+
+    return encryptedText
+
+
+####################################################
+#
+#   Main
+#
+####################################################
+
 
 def main():
-	keyspace = "abcdefghijklmnopqrstuvwxyz"
+    keyspace = "abcdefghijklmnopqrstuvwxyz"
 
-	coprimes = coprimeGen(len(keyspace))
-	
-	inputs = getInputs()
+    coprimes = coprime_gen(len(keyspace))
 
-	e = encryptString('affine', inputs[0], inputs[1], keyspace, True)
-	
-	print(e)
+    inputs = get_inputs(keyspace,coprimes)
+    keys = inputs[:2]  # keys are first 2 numbers
+
+    encrypt_decrypt = inputs[2]
+
+    message = inputs[3]
+
+    if keys[0] not in coprimes:
+        raise ValueError("Error! "+str(keys[0]+" is not coprime with " + str(len(keyspace))))
+
+    print(
+        encrypt_decrypt_string(message, keys[0], keys[1], keyspace, encrypt_decrypt)
+    )
+
 
 main()
